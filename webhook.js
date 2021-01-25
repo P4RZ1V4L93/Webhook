d55ac764a722ac0e83843603a6ef166ed0392b49
P4RZ1V4L93 <76917939+P4RZ1V4L93@users.noreply.github.com>
2021-01-25 20:46:51 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,16 +1,14 @@
-{
-  "name": "test webhook",
-  "type": 1,
-  "channel_id": "802168596544880643",
-  "token": "h2_MSnY5j8fCTZ9_cA1zl7J8ulF0vcbicD7hXGfYmb2FC51f9aS8WveCm1HsN-a0yG_p",
-  "avatar": null,
-  "guild_id": "802168596008402964",
-  "id": "803263014845415466",
-  "message": "test"
-  "user": {
-    "username": "Roooo",
-    "discriminator": "8903",
-    "id": "356721043613810689",
-    "avatar": "b004ec1740a63ca06ae2e14c5cee11f3"
-  }
-}
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
+
+const embed = new Discord.MessageEmbed()
+	.setTitle('Some Title')
+	.setColor('#0099ff');
+
+webhookClient.send('Webhook test', {
+	username: 'some-username',
+	avatarURL: 'https://i.imgur.com/wSTFkRM.png',
+	embeds: [embed],
+});
